ff286ed202dfb99f5a74735f6f7ca2c3ae135e8e
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-07-05 23:15:52 +0100

    Added contractUtils which calls the safeMint smart contract function using its .env variables

--- backend-api/src/utils/contractUtils.js
+++ backend-api/src/utils/contractUtils.js
@@ -1,1 +1,26 @@
+import { ethers } from 'ethers';
+import contractABI from '../../artifacts/contracts/TicquetteNFT.sol/TicquetteNFT.json').abi;
+dotenv.config();
+
+const provider = new ethers.providers.JsonRpcProvider(process.env.INFURA_URL);
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+const contract = new ethers.Contract(process.env.CONTRACT_ADDRESS, contractABI, wallet);
+
+exports.safeMintNFT = async (to, tokenURI, meta) => {
+  const tx = await contract.safeMint(
+    to,
+    tokenURI,
+    meta.useOfSpace,
+    meta.dimensionOfSpace,
+    meta.lga,
+    meta.state,
+    meta.country,
+    meta.durationOfLease
+  );
+
+  const receipt = await tx.wait();
+  const tokenId = receipt.events.find(e => e.event === 'TicquetteMinted').args.tokenId.toString();
+
+  return { tokenId, receipt };
+};
 
