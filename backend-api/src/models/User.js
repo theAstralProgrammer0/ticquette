63732149de8820a202c926ea63e7c660a0d7a029
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-07-06 09:31:19 +0100

    Added models/User for specifying the User schema using mongoose

---
+++ backend-api/src/models/User.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  walletAddress: {
+    type: String,
+    required: true,
+    unique: true
+  }
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+export default User;
+
