72c4fb11749c79638e2f87bc31813031d3787648
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-07-06 09:38:58 +0100

    Completed appController

--- backend-api/src/controllers/appController.js
+++ backend-api/src/controllers/appController.js
@@ -3,3 +3,17 @@ import dbClient from '../utils/db';
 import User from '../models/User';
 import NFT from '../models/NFT';
 
+export const getStatus = async (req, res) => {
+  const status = {
+    redis: redisClient.isAlive(),
+    db: dbClient.isAlive()
+  };
+  res.status(200).json(status);
+};
+
+export const getStats = async (req, res) => {
+  const users = await User.countDocuments();
+  const nfts = await NFT.countDocuments();
+  res.status(200).json({ users, nfts });
+};
+
