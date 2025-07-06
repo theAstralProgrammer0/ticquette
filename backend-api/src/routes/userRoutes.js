eca121d6ec30e989c64ee16cd95761915866ef66
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-07-06 09:33:24 +0100

    Added routes/userRoutes for specifying the endpoint and function within user controllers for handling the endpoints service

---
+++ backend-api/src/routes/userRoutes.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { Router } from 'express';
+import { postNewUser } from '../controllers/userController';
+
+const router = Router();
+
+router.post('/users', postNewUser);
+
+export default router;
+
