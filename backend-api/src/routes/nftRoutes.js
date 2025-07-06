39fb622165e3a806bbb4ed760456f568f5ed7031
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-07-06 09:32:38 +0100

    Added routes/nftRoutes for specifying the endpoint and function within controllers for handling the endpoints service

--- backend-api/src/routes/nftRoutes.js
+++ backend-api/src/routes/nftRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { Router } from 'express';
-import { mintNFT } from '../controllers/nftController';
+import { mintNFT, getAllNFTs, getNFTByTokenId } from '../controllers/nftController';
 
 const router = Router();
 
 router.post('/mint', mintNFT);
+router.get('/nfts', getAllNFTs);
+router.get('/nft/:tokenId', getNFTByTokenId);
 
 export default router;
 
