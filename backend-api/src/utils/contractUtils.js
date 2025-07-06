f94e716139129f141604da7b12399053884e6f93
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-07-06 08:38:18 +0100

    Modified contractUtils.js to initiate the contract and return the tokenId, receipt, and expirationDate

--- backend-api/src/utils/contractUtils.js
+++ backend-api/src/utils/contractUtils.js
@@ -1,26 +1,13 @@
 import { ethers } from 'ethers';
-import contractABI from '../../artifacts/contracts/TicquetteNFT.sol/TicquetteNFT.json').abi;
+import contractABI from '../../../smart-contract/artifacts/contracts/TicquetteNFT.sol/TicquetteNFT.json';
+import dotenv from 'dotenv';
 dotenv.config();
 
-const provider = new ethers.providers.JsonRpcProvider(process.env.INFURA_URL);
-const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-const contract = new ethers.Contract(process.env.CONTRACT_ADDRESS, contractABI, wallet);
+const provider = new ethers.JsonRpcProvider(process.env.SEPOLIA_URL);
+const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+const contract = new ethers.Contract(process.env.CONTRACT_ADDRESS, contractABI.abi, signer);
 
-exports.safeMintNFT = async (to, tokenURI, meta) => {
+export const mintOnBlockchain = async (walletAddress, metadataCID, metadata) => {
   const tx = await contract.safeMint(
-    to,
-    tokenURI,
-    meta.useOfSpace,
-    meta.dimensionOfSpace,
-    meta.lga,
-    meta.state,
-    meta.country,
-    meta.durationOfLease
-  );
-
-  const receipt = await tx.wait();
-  const tokenId = receipt.events.find(e => e.event === 'TicquetteMinted').args.tokenId.toString();
-
-  return { tokenId, receipt };
-};
+    walletAddress,
 
