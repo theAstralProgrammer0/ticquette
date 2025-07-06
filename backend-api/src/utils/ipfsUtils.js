5c4cf2f6e894497394b58baf57b93970be577a73
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-07-06 08:40:01 +0100

    Modified ipfsUtils.js. Used pinata sdk and its library functions to pinJSONToIPFS and return the IpfsHash

--- backend-api/src/utils/ipfsUtils.js
+++ backend-api/src/utils/ipfsUtils.js
@@ -1,18 +1,12 @@
-import axios from 'axios';
-import FormData from 'form-data');
+import pinataSDK from '@pinata/sdk';
+import dotenv from 'dotenv';
 dotenv.config();
 
-const PINATA_BASE_URL = 'https://api.pinata.cloud/pinning';
+const pinata = new pinataSDK({ pinataJWTKey: process.env.PINATA_JWT });
 
-exports.pinJSONToIPFS = async (json) => {
-  const url = `${PINATA_BASE_URL}/pinJSONToIPFS`;
-  const res = await axios.post(url, json, {
-    headers: {
-      'Content-Type': 'application/json',
-      pinata_api_key: process.env.PINATA_API_KEY,
-      pinata_secret_api_key: process.env.PINATA_SECRET_API_KEY
-    }
-  });
-  return res.data.IpfsHash;
+export const pinMetadataToIPFS = async (metadata) => {
+  const options = { pinataMetadata: { name: 'Ticquette Metadata' } };
+  const result = await pinata.pinJSONToIPFS(metadata, options);
+  return result.IpfsHash;
 };
 
