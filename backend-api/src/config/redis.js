3639b3d0d08698e0121a9e6a97281559be030758
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-07-06 10:28:27 +0100

    Added config/redis.js specifying the redis client config params and constructor

---
+++ backend-api/src/config/redis.js
@@ -0,0 +1,29 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => console.error('Redis Client Error', err));
+    this.client.connect();
+  }
+
+  isAlive() {
+    return this.client.isReady;
+  }
+
+  async get(key) {
+    return this.client.get(key);
+  }
+
+  async set(key, val, duration) {
+    await this.client.setEx(key, duration, value);
+  }
+
+  async del(key) {
+    await this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
+
