73c9b4670edc50bf3b5e2619a3ea6fe671411fcc
bn-yussuph <sulehajjaajj@gmail.com>
2025-07-09 11:46:56 +0000

    A little typo in userRoutes.js

--- backend-api/src/routes/userRoutes.js
+++ backend-api/src/routes/userRoutes.js
@@ -1,25 +1,11 @@
 import { Router } from 'express';
-<<<<<<< HEAD
-import { postNewUser,
-            getUsers,
-            getUserById } from '../controllers/userController.js';
-=======
 import { postNewUser, getAllUsers, getUserById } from '../controllers/userController.js';
->>>>>>> master
 
 const userRouter = Router();
 
-<<<<<<< HEAD
-router.get('/users', getUsers);
-router.get('/user/:id', getUserById);
-router.post('/users', postNewUser);
-
-export default router;
-=======
 userRouter.post('/users', postNewUser);
 userRouter.get('/users', getAllUsers);
 userRouter.get('/user/:id', getUserById);
 
 export default userRouter;
 
->>>>>>> master
